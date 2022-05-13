e5836eedf2bfef983b22a59cd2a9acf4b9480b67
CongLiu <dibatian@live.com>
2022-05-13 09:28:04 +0800

    commit on 0513

--- Seurat_multimodal.R
+++ Seurat_multimodal.R
@@ -1,4 +1,5 @@
 # 10x multiome kit: scRNA-seq+scATAC-seq
+# 单细胞多组学整合分析的方法
 
 
 library(ggplot2)
--- Signnac.R
+++ Signnac.R
@@ -1,3 +1,25 @@
+## ---------------------------
+##
+## Script name: title here
+##
+## Purpose of script:
+##
+## Author: LiuCong
+##
+## Date Created: 2022-03-29
+##
+## Copyright (c) cliu, 2022
+## Email: dibatian@live.com
+##
+## ---------------------------
+##
+## Notes: Signac is designed for the analysis of single-cell chromatin data, 
+## including scATAC-seq, single-cell targeted tagmentation methods such as 
+## scCUT&Tag and scACT-seq, and multimodal datasets that jointly measure chromatin 
+## state alongside other modalities
+##
+## ---------------------------
+
 
 library(Signac)
 
